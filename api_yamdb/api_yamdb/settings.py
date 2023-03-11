72d3dd18748c1c5f311a48870cc150830e8219a2
Xzenoff <xzenonoff@gmail.com>
2023-03-11 14:51:42 +0300

    change settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv(
-    'SECRET_KEY', default='asd_fdojf_fdsafn_kfansskl-dsaknlvt45357fhrt'
+    'SECRET_KEY', default='asd_fdojf_fdsafn_kfansskl-dsaknlrvt45357fhrt'
 )
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
